6f92ea8e649141836b84be64b4d5734f866bb8c6
fox <foxiswho@gmial.com>
2020-03-15 17:03:21 +0800

    update

---
+++ src/main/java/com/foxwho/springbootdroolsdemo/SpringBootDroolsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.foxwho.springbootdroolsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDroolsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDroolsDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foxwho/springbootdroolsdemo/SpringBootDroolsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foxwho.springbootdroolsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class SpringBootDroolsDemoApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
